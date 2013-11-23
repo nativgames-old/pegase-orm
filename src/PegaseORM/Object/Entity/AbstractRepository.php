8ab4f17074cf5bf95b25f4cfcea0ad21080f676b
Pierre Emmanuel Lallemant <pe.lallemant@hotmail.fr>
2013-11-23 19:23:19 +0100

    abstract_repository has a new usefull method: convert_to_entity

--- src/PegaseORM/Object/Entity/AbstractRepository.php
+++ src/PegaseORM/Object/Entity/AbstractRepository.php
@@ -11,5 +11,19 @@ class AbstractRepository {
     $this->db = $db;
     $this->table = $table;
   }
+
+  public function convert_to_entity($to_convert) {
+    $name = $this->table->get_classname();
+    $entity = new $name();
+      
+    foreach($this->table->get_columns() as $col) {
+      $method = "set_";
+      $method .= $col[0]; // var name
+
+      $value = call_user_func(array($entity, $method), $to_convert[$col[0]]);
+    }
+
+    return $entity;
+  }
 }
 
